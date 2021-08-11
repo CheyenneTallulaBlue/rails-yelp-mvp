7764d58b97b43ac86d3808ca0b7954b2c5e8e5ef
cheyenne simoleit <cheyenne@simoleit.com>
2021-08-11 16:10:21 +0200

    DOnezies

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,11 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first) "chinese", "italian", "japanese", "french", "belgian"]
+
+
+restaurants = Restaurant.create([{name: "Wuhwuh's", address: "45 Long avenue", category: "chinese"},
+{name: "Lucy's Cupcake Boat", address: "238 Canal Walk", category: "french"},
+{name: "Gazzo", address: "23 Princess Street", category: "italian"},
+{name: "Harijuku", address: "43 Brunstfield Place", category: "japanese"},
+{name: "de Wouters", address: "38 Collingwood way", category: "belgian"}])
